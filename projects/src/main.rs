b4040e6e88ed357f8c742d67c60be56300bc7936
DaaOtt <daaott@gmail.com>
2016-08-10 10:50:15 -0700

    fibonacci

--- projects/src/main.rs
+++ projects/src/main.rs
@@ -1,7 +1,8 @@
 fn main() {
     println!("Hello, world!");
     println!("Pi(100): {}", pi(100));
-    println!("e(10000000000): {}", e(10000000000));
+    println!("terrible e(10000000000): {}", e(10000000000));
+    println!("fib(10): {:?}", fib(10))
 }
 
 fn pi(n: u64) -> f64 {
@@ -20,4 +21,14 @@ fn pi(n: u64) -> f64 {
 fn e(n: u64) -> f64 {
     let n = n as f64;
     (1.0 + 1.0/n).powf(n)
+}
+
+fn fib(n: u64) -> Vec<u64> {
+    let mut result = vec![1u64, 1u64];
+    for _ in 2..n { // Should probably be 3..n + 1
+        let len = result.len();
+        let sum = result[len - 1] + result[len - 2];
+        result.push(sum);
+    }
+    result
 }
